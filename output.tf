095a42f3708b0708ac29a0971633f436c7d612f2
christophefromparis <christophe.cosnefroy@gmail.com>
2019-04-18 14:05:49 +0200

    Cleaning

--- output.tf
+++ output.tf
@@ -10,13 +10,13 @@ output "development_namespace" {
   value = "${var.development_namespace}"
 }
 
-output "fqdn_suffix" {
+/*output "fqdn_suffix" {
   value = "${data.terraform_remote_state.infra.fqdn_suffix}"
 }
 
 output "creator_label" {
   value = "${data.terraform_remote_state.infra.creator_label}"
-}
+}*/
 
 output "aws_account" {
   value = "${var.aws_account}"
