b5f8ecea9d579514de64528d2b95ce198db48a0e
christophefromparis <christophe.cosnefroy@gmail.com>
2019-04-18 13:51:43 +0200

    Typo

--- external-dns.tf
+++ external-dns.tf
@@ -1,5 +1,5 @@
 locals {
-  values-filename = "${path.module}/files/${var.dns_provider}-external-dns-values.yaml"
+  values-filename = "${path.module}/files/${var.dns_provider}-external-dns-values.yml"
 }
 
 # ---  We prepare the external-dns values.yaml ---
