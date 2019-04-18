6f2e8b31dc8a980cd251644d809cf11cf11d30a4
christophefromparis <christophe.cosnefroy@gmail.com>
2019-04-18 14:22:04 +0200

    Add nginx-ingress helm

---
+++ nginx-ingress.tf
@@ -0,0 +1,57 @@
+# ---  We prepare the Nginx values.yaml ---
+data "template_file" "nginx-ingress-template" {
+  template = "${file("${path.module}/files/nginx-ingress-values.yaml")}"
+}
+
+#--- NGINX Ingress Controller Helm ---
+resource "helm_release" "nginx_controller" {
+  name      = "nginx-controller"
+  chart     = "stable/nginx-ingress"
+  namespace = "${var.global_namespace}"
+  version   = "${lookup(var.helm_version, "nginx-ingress")}"
+  timeout   = "600"
+
+  values = [
+    "${data.template_file.nginx-ingress-template.rendered}"
+  ]
+
+  set {
+    name  = "rbac.create"
+    value = "true"
+  }
+  set {
+    name  = "controller.kind"
+    value = "DaemonSet"
+  }
+  set {
+    name  = "controller.stats.enabled"
+    value = "true"
+  }
+  set {
+    name  = "controller.metrics.enabled"
+    value = "true"
+  }
+  set {
+    name  = "controller.service.enableHttps"
+    value = "true"
+  }
+  set {
+    name  = "controller.service.enableHttp"
+    value = "true"
+  }
+  # to preserve the IP source (and so enabled the whitelisting)
+  set {
+    name  = "controller.service.externalTrafficPolicy"
+    value = "Local"
+  }
+  set {
+    name  = "controller.publishService.enabled"
+    value = "true"
+  }
+  set {
+    name  = "controller.publishService.pathOverride"
+    value = "${var.global_namespace}/nginx-controller-nginx-ingress-controller"
+  }
+
+  depends_on = ["null_resource.refresh_chart_repo", "helm_release.cert-manager", "helm_release.external-dns"]
+}
