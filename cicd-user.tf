51ce19981c2a75fab2dad86170d489d5dae851ae
christophefromparis <christophe.cosnefroy@gmail.com>
2019-04-18 14:22:26 +0200

    Add cicd-user (but I don't use it)

---
+++ cicd-user.tf
@@ -0,0 +1,49 @@
+resource "kubernetes_service_account" "bitbucket-pipeline" {
+  metadata {
+    name      = "${var.pipeline_sa_name}"
+    namespace = "${var.development_namespace}"
+  }
+
+  depends_on = ["kubernetes_namespace.dev"]
+}
+
+data "kubernetes_secret" "bitbucket-pipeline" {
+  metadata {
+    name      = "${kubernetes_service_account.bitbucket-pipeline.default_secret_name}"
+    namespace = "${var.development_namespace}"
+  }
+}
+
+resource "kubernetes_role" "deploy" {
+  metadata {
+    name      = "${var.deploy_role_name}"
+    namespace = "${var.development_namespace}"
+  }
+  rule {
+    api_groups = ["extensions", "apps"]
+    resources  = ["deployments"]
+    verbs      = ["get", "update", "patch", "create"]
+  }
+}
+
+resource "kubernetes_role_binding" "bitbucket-pipeline-deploy" {
+  metadata {
+    name      = "bitbucket-pipeline-deploy"
+    namespace = "${var.development_namespace}"
+  }
+  role_ref {
+    api_group = "rbac.authorization.k8s.io"
+    kind      = "Role"
+    name      = "${var.deploy_role_name}"
+    namespace = "${var.development_namespace}"
+  }
+  subject {
+    kind      = "ServiceAccount"
+    name      = "${var.pipeline_sa_name}"
+    namespace = "${var.development_namespace}"
+  }
+
+/*  provisioner "local-exec" {
+    command = "kubectl get secret ${kubernetes_service_account.bitbucket-pipeline.default_secret_name} -n \"${var.development_namespace}\" -o jsonpath=\"{['data']['ca\\.crt']}\" | base64 -d >> ca.crt"
+  }*/
+}
