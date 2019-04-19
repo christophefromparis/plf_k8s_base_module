71f83e13de199f4a2dd39d90a78343659a48db1e
christophefromparis <christophe.cosnefroy@gmail.com>
2019-04-19 15:03:53 +0200

    Correct variable

--- cicd-user.tf
+++ cicd-user.tf
@@ -1,7 +1,7 @@
 resource "kubernetes_service_account" "bitbucket-pipeline" {
   metadata {
     name      = "${var.pipeline_sa_name}"
-    namespace = "${var.development_namespace}"
+    namespace = "${kubernetes_namespace.dev.id}"
   }
 
   depends_on = ["kubernetes_namespace.dev"]
@@ -10,14 +10,14 @@ resource "kubernetes_service_account" "bitbucket-pipeline" {
 data "kubernetes_secret" "bitbucket-pipeline" {
   metadata {
     name      = "${kubernetes_service_account.bitbucket-pipeline.default_secret_name}"
-    namespace = "${var.development_namespace}"
+    namespace = "${kubernetes_namespace.dev.id}"
   }
 }
 
 resource "kubernetes_role" "deploy" {
   metadata {
     name      = "${var.deploy_role_name}"
-    namespace = "${var.development_namespace}"
+    namespace = "${kubernetes_namespace.dev.id}"
   }
   rule {
     api_groups = ["extensions", "apps"]
@@ -29,18 +29,18 @@ resource "kubernetes_role" "deploy" {
 resource "kubernetes_role_binding" "bitbucket-pipeline-deploy" {
   metadata {
     name      = "bitbucket-pipeline-deploy"
-    namespace = "${var.development_namespace}"
+    namespace = "${kubernetes_namespace.dev.id}"
   }
   role_ref {
     api_group = "rbac.authorization.k8s.io"
     kind      = "Role"
     name      = "${var.deploy_role_name}"
-    namespace = "${var.development_namespace}"
+    namespace = "${kubernetes_namespace.dev.id}"
   }
   subject {
     kind      = "ServiceAccount"
     name      = "${var.pipeline_sa_name}"
-    namespace = "${var.development_namespace}"
+    namespace = "${kubernetes_namespace.dev.id}"
   }
 
 /*  provisioner "local-exec" {
