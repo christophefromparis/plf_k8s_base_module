3f525b72a2dd6e65956ee66d7f151d3a3a396d4a
christophefromparis <christophe.cosnefroy@gmail.com>
2019-04-23 14:52:48 +0200

    Clean output

--- output.tf
+++ output.tf
@@ -1,19 +1,11 @@
-/*output "fqdn_suffix" {
-  value = "${data.terraform_remote_state.infra.fqdn_suffix}"
-}
-
-output "creator_label" {
-  value = "${data.terraform_remote_state.infra.creator_label}"
-}*/
-
 output "aws_account" {
   value = "${var.aws_account}"
 }
 
-/*output "bitbucket_data" {
-  value = "${data.kubernetes_secret.bitbucket-pipeline.data}"
-}*/
-
 output "monitoring_ns" {
   value = "${kubernetes_namespace.monitoring.id}"
+}
+
+output "test" {
+  value = "${var.gcp_credentials}"
 }
